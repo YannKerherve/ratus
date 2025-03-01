39ff5e57a2464435ccee8e1da0163896e3144838
Yann Kerherve <ya.kerherve@gmail.com>
2025-03-01 16:43:36 +0100

    Add files via upload

---
+++ pluginConfig.ts
@@ -0,0 +1,16 @@
+import type { ExternalPluginConfig } from '@windy/interfaces';
+
+const config: ExternalPluginConfig = {
+    name: 'windy-plugin-gpsfromcomTCP',
+    version: '0.0.01',
+    icon: '🛰️',
+    title: 'GPS from COM',
+    description: 'GPS Position from TCP port.',
+    author: 'Yann Kerherve (ENSM)',
+    repository: 'https://github.com/windycom/windy-plugin-template',
+    desktopUI: 'rhpane',
+    mobileUI: 'fullscreen',
+    routerPath: '/gpsfromtcp',
+};
+
+export default config;
