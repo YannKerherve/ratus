2e023c1366287e19a011a543af3a0cda36ab354d
Yann Kerherve <ya.kerherve@gmail.com>
2025-03-02 12:14:21 +0100

    Update pluginConfig.ts

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-gpsfromtcp',
-    version: '0.1.11',
+    version: '0.1.12',
     icon: '🛰️',
     title: 'GPS from TCP',
     description: 'GPS Position from TCP port.',
