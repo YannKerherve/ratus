e5c5a93f39bccab6f2d867119641ef2031eac7ac
Yann Kerherve <ya.kerherve@gmail.com>
2025-03-06 22:07:16 +0100

    Update pluginConfig.ts

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -1,13 +1,13 @@
 import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
-    name: 'windy-plugin-gpsfromtcp',
-    version: '0.2.2',
+    name: 'windy-plugin-gpsfromtcplcc',
+    version: '0.1.1',
     icon: '🛰️',
-    title: 'GPS from TCP',
-    description: 'GPS Position from TCP port',
+    title: 'GPS from TCP LCC',
+    description: 'GPS Position from TCP LCC',
     author: 'Yann Kerherve (ENSM)',
-    repository: 'https://github.com/YannKerherve/Windy-plugin-GPS-from-TCP',
+    repository: 'https://github.com/YannKerherve/ratus',
     desktopUI: 'rhpane',
     mobileUI: 'fullscreen',
     routerPath: '/gpsfromtcp',
