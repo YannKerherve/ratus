39d9507c53c658abebcd1ffcd650f9f4d57125ee
Yann Kerherve <ya.kerherve@gmail.com>
2025-03-08 14:06:53 +0100

    Update pluginConfig.ts

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-gpsfromtcplcc',
-    version: '0.1.7',
+    version: '0.1.8',
     icon: '🛰️',
     title: 'GPS from TCP LCC',
     description: 'GPS Position from TCP LCC',
